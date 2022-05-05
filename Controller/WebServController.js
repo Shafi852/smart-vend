8bcbc6311f375c672f898572f24404149e6ada6a
Shafi852 <shafivp78@gmail.com>
2022-05-05 19:23:40 +0530

    Main Stream

---
+++ Controller/WebServController.js
@@ -0,0 +1,18 @@
+var bodyParser = require('body-parser');
+var lastJson = NaN
+
+module.exports={ postHandler : function(app){
+    // handle webhook json and display on website
+    app.post('/', (req,res)=>{
+        lastJson = req;
+           
+    })
+},
+
+lastJson : function(){
+    lastJson;
+}
+
+
+}
+lastJson.status
